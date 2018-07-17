3b3d746b14c7afb682d6b9e243bb770936251f04
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-07-16 17:51:34 -0700

    added second list

--- app.js
+++ app.js
@@ -35,6 +35,50 @@ for(var i = 0; i < timeOfDay.length; i++) {
 
 console.log(firAndPike);
 
-var timeSelect = document.getElementById('timeOfDay');
+var timeOfDay = document.getElementById('firAndPike');
+timeOfDay.appendChild(ulEl);
+
+
+
+
+
+//Start new list for Seatac
+
+
+var timeOfDay = ['6am', '7am','8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var seatacAir = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  AvgCookieSale: 1.2,
+  cookiePerHour : [], //store the cookies
+  getCustomerPerHour: function getCustomerPerHour(min,max){
+    return Math.random() *(max-min) + min;
+  },
+
+};
+
+
+for(var j = 0; j < timeOfDay.length; j ++) {
+  var cusPerHour = seatacAir.getCustomerPerHour(seatacAir.maxCustomer,seatacAir.minCustomer);
+  var cookiePerHour = Math.ceil(seatacAir.AvgCookieSale * cusPerHour);
+
+  seatacAir.cookiePerHour.push(cookiePerHour);
+
+}
+console.log(j);
+
+
+var ulEl = document.createElement('ul');
+
+for(var i = 0; i < timeOfDay.length; i++) {
+  var liEl = document.createElement('li');
+  liEl.textContent = timeOfDay[i] + ' ' + ' ' + seatacAir.cookiePerHour[i];
+  ulEl.appendChild(liEl);
+}
+
+console.log(seatacAir);
+
+var timeSelect = document.getElementById('seatacAir');
 timeSelect.appendChild(ulEl);
 
