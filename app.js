bc8e7fa0b82814f52026c088263161a6abc5ad44
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-07-17 12:43:59 -0700

    updated some stuff

--- app.js
+++ app.js
@@ -110,10 +110,6 @@ function finalTotalTwo() {
 finalTotalTwo();
 
 
-
-
-
-
 //Seattle Center 
 
 var timeOfDay = ['6am', '7am','8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
