0dab7c54499eb1e39747b9d645c9a942a03d924a
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-07-19 15:10:30 -0700

    updated table with totals on the bottem

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 'use strict';
 //object today is to create a form that allows the user to input data and it to update on the table and create a new table line.
 
-//event listeners when you refer to this
-//var cookiesPerStore = 0;
-//salmonCookies.push(this);
 
-//this is the breaking point of where lab from yesterday started. This is where we created the table itself
-var tblEl;
 var timeOfDay = ['6am', '7am','8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var salmonCookies = [];
-var total = ['Totals'];
-
-
-//var dailyTotal= timeOfDay
+var tblEl = document.getElementById('table');
 
 function CookieTable(name,minCustomer,maxCustomer,AvgCookieSale) {
   this.name = name;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.AvgCookieSale = AvgCookieSale;
-  this.cookiePerHour = [],
-  this.cookiesPerStore = 0,
+  this.cookiePerHour = [];
+  this.cookiesPerStore = 0;
   this.customersPerHour = [];
+
   salmonCookies.push(this);
+  //createTable();
+  bombTotalRow();
+  this.render();
+  createEndTotal();
+
 }
 
-new CookieTable('First and Pike',23, 65,6.3);
-new CookieTable('Seatac Airport',3,24,1.2);
-new CookieTable('Seattle Center',11,38,3.7);
-new CookieTable('Alki',2,16,4.6);
-new CookieTable('Capitol Hill',20,38,2.3);
-
+//---------Generates random number
 CookieTable.prototype.generateRandom = function() {
   return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
 };
-
+//----------Calculate Customerss
 CookieTable.prototype.calculateCustomers = function() {
-  for (var cats of timeOfDay) {
+  for (var a of timeOfDay) {
     var rando = this.generateRandom();
     this.customersPerHour.push(rando);
   }
 };
+//------------ CALCULATE SALES
 CookieTable.prototype.calculateSales = function() {
   this.calculateCustomers();
   for (var numCustomers of this.customersPerHour) {
@@ -53,46 +47,20 @@ CookieTable.prototype.calculateSales = function() {
 };
 
 
-function createTable() {
-  //header row
-  var tblEl = document.getElementById('table');
-  var trHeaderEl = document.createElement('tr');
-  var thBlankEl = document.createElement ('th');
-  thBlankEl.textContent = 'Store name';
-  trHeaderEl.appendChild(thBlankEl);
 
+//--------RENDER FUNCTION Row others
 
-  for (var idx = 0; idx < timeOfDay.length; idx++) {
-    var thEl = document.createElement('th');
-    thEl.textContent = timeOfDay[idx];
-    trHeaderEl.appendChild(thEl);
-  }
-  var thTotalEl = document.createElement('th');
-  thTotalEl.textContent = 'Daily Total';
-  trHeaderEl.appendChild(thTotalEl);
-
-  tblEl.appendChild(trHeaderEl);
-  
-}
-function createEndTotal(){
- 
-  
-  
-  
-  //document.getElementById('').appendChild(tblEl);
-}
 
 CookieTable.prototype.render = function(){
-  // console.log(this.cookiePerHour);
   this.calculateSales();
-  tblEl = document.getElementById('table');
-  var trStoreEl = document.createElement('tr');
 
+  //var tblEl = document.getElementById('table');
+  var trStoreEl = document.createElement('tr');
   var tdNameEl = document.createElement('td');
   tdNameEl.textContent = this.name;
   trStoreEl.appendChild(tdNameEl);
 
-  for (var i = 0; i < timeOfDay.length; i ++){
+  for (var i = 0; i < timeOfDay.length; i++){
     var tdDataEl = document.createElement('td');
     tdDataEl.textContent =this.cookiePerHour[i];
 
@@ -106,60 +74,105 @@ CookieTable.prototype.render = function(){
   tblEl.appendChild(trStoreEl);
 
 };
-//for (var y = 0; y < cookiePerHour.length 
+function createTable() { //rowfirst
+  //-----------------HEADER row
+  //this.calculateSales();
 
-createTable(); // this actually creates the table
+  // var tblEl = document.getElementById('table');
+  //var trHeaderEl = document.createElement('hour-row');
+  var trEl =document.createElement('tr');
 
-//finally run render
+  var thBlankEl = document.createElement ('td');
+  thBlankEl.textContent = 'Stores';
+  trEl.appendChild(thBlankEl);
+
+
+  for (var idx = 0; idx < timeOfDay.length; idx++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = timeOfDay[idx];
+    trEl.appendChild(thEl);
+    //trEl.appendChild(trEl);
+  }
+
+  var thTotalEl = document.createElement('td');
+  thTotalEl.textContent = 'Total';
+  trEl.appendChild(thTotalEl);
+  tblEl.appendChild(trEl);
+
+}
+createTable();
+
+//this actually creates the table
+
+//finally run RENDER--------------
 for (var store of salmonCookies) {
   store.render();
 }
 
+//----------create FOOTER total
 
-var pClickEl = document.getElementById('p-click');
+function createEndTotal(){
+  var trTotalEl = document.createElement('tr');
+  trTotalEl.id = 'total-row';
+  var tdFirstEle = document.createElement('td');
+  tdFirstEle.textContent = 'Totals';
+  trTotalEl.appendChild(tdFirstEle);
 
-function clicked() {
-    
-  console.log('Clicked');
+  var grandTotal = 0;
+
+  for(var k = 0; k < timeOfDay.length; k++) {
+    var total = 0;
+
+    for (var j = 0; j < salmonCookies.length; j++){
+      console.log(salmonCookies[j].cookiePerHour[k]);
+      total += salmonCookies[j].cookiePerHour[k];
+    }
+    console.log(total);
+    var tdDataEle = document.createElement('td');
+    tdDataEle.textContent = total;
+    trTotalEl.appendChild(tdDataEle);
+    grandTotal += total;
+  }
+
+  var tdGrandtotalEle = document.createElement('td');
+  tdGrandtotalEle.textContent = grandTotal;
+  trTotalEl.appendChild(tdGrandtotalEle);
+
+  tblEl.appendChild(trTotalEl);
 }
 
-pClickEl.addEventListener('click',clicked);
+function bombTotalRow(){
+  var totalRow = document.getElementById('total-row');
+  if (totalRow){
+    totalRow.remove();
+  }
+}
 
-//form data
+//---------- This is the click button that refreshes the page
+// var pClickEl = document.getElementById('p-click');
+
+// function clicked() {
+//   console.log('Clicked');
+// }
+
+// clicked(formEl);
+// pClickEl.addEventListener('click',clicked);
+
+//----------FORM DATA that takes user input
 
 var formEl = document.getElementById('main-form');
-
-formEl.addEventListener('submit', dataEntry);
-function dataEntry(event) {
+formEl.addEventListener('submit', function(event){
   event.preventDefault();
-  console.log(event.target);
+
   var name = event.target.Store.value;
   var minCustomer = event.target.Min.value;
   var maxCustomer = event.target.Max.value;
   var AvgCookieSale = event.target.AvgCookieSale.value;
-
-  console.log(name);
-  
-  var tableAdd = new CookieTable (name, minCustomer,maxCustomer,AvgCookieSale);
-  tableAdd.render();
-}
-clicked(formEl);
-
-
-// function totalperHour(){
-//   var number = 0;
-//   //console.log(timeOfDay);
-//   //console.log(salmonCookies);
-//   for(var store of salmonCookies){
-//     number += salmonCookies[store].cookiePerHour[hour];
-//     console.log(total);
-//   }
-
-//   // for(var store in salmonCookies){
-//   //   console.log(cookiePerHour);
-//   //   console.log(salmonCookies[store].cookiePerHour[0]);
-//     for(var hour in timeofDay){
-//       number += timeofDay[store].cookiePerHour[hour];
-
-//   totalperHour();
+  new CookieTable(name,minCustomer,maxCustomer,AvgCookieSale);
+});
+new CookieTable('First and Pike',23, 65,6.3);
+new CookieTable('Seatac Airport',3,24,1.2);
+new CookieTable('Seattle Center',11,38,3.7);
+new CookieTable('Alki',2,16,4.6);
+new CookieTable('Capitol Hill',20,38,2.3);
 
